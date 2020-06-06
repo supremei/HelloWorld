ab0edc3bda97c5727ae9daf05f83d113367a482a
supremeii <1394575892@qq.com>
2020-06-06 16:42:10 +0800

    test

--- src/com/cqust/hellojava/HelloJava.java
+++ src/com/cqust/hellojava/HelloJava.java
@@ -2,7 +2,7 @@ package com.cqust.hellojava;
 
 public class HelloJava {
 	public static void main(String[] args) {
-		System.out.println("HELLO JAVA!");
+		System.out.println("HELLO JAVA! 随便改一改");
 		System.out.println("敲入syso, alt+/");
 	}
 }
