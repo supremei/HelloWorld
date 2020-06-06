fdd1f807ae767f160df5d6cdfa3b40d09623ab4c
supremeii <1394575892@qq.com>
2020-06-06 16:35:23 +0800

    test

---
+++ src/com/cqust/hellojava/HelloJava.java
@@ -0,0 +1,8 @@
+package com.cqust.hellojava;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		System.out.println("HELLO JAVA!");
+		System.out.println("«√»Îsyso, alt+/");
+	}
+}
